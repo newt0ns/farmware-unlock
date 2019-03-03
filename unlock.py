b59b08b9a89afe171851435145b85248daa75dc7
newt0ns <nate.stuart@gmail.com>
2019-03-02 17:46:36 -0800

    Update unlock.py

--- unlock.py
+++ unlock.py
@@ -3,6 +3,6 @@
 '''farmware-unlock
 '''
 
-from farmware_tools import get_config_value, device
+from farmware_tools import device
 device.emergency_unlock()
 device.log(message='farware-unlock exectued", message_type='success')
